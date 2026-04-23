f1a4f32026ff0b876253bb19334985a4ad71d8e8
terracodum <nikitakharlov007@gmail.com>
2026-04-23 21:46:31 +0600

    feat(angle): add Angle class with tests

---
+++ SpaceBattle.Lib/Angle.cs
@@ -0,0 +1,52 @@
+﻿namespace StarWars.Lib;
+
+public class Angle
+{
+    private readonly int _numerator;
+    private static readonly int Denominator = 8;
+
+    public Angle(int num)
+    {
+        _numerator = (num % Denominator + Denominator) % Denominator;
+    }
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public static implicit operator double(Angle angle)
+    {
+        return ((double)angle._numerator / Denominator) * 2 * Math.PI;
+    }
+
+    public static Angle operator +(Angle a1, Angle a2)
+    {
+        return new Angle(a1._numerator + a2._numerator);
+    }
+
+    public static bool operator ==(Angle a1, Angle a2)
+    {
+        return a1.Equals(a2);
+    }
+
+    public static bool operator !=(Angle a1, Angle a2)
+    {
+        return !(a1 == a2);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Angle other)
+        {
+            return false;
+        }
+
+        return _numerator.Equals(other._numerator);
+    }
+
+    public override int GetHashCode()
+    {
+        return _numerator.GetHashCode();
+    }
+}
+
