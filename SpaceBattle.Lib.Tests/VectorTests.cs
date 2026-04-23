6ad38184d394f1b3791ec6abe8e61ae81941e51b
terracodum <nikitakharlov007@gmail.com>
2026-04-23 16:24:36 +0600

    test(vector): add edge case tests for 100% coverage

--- SpaceBattle.Lib.Tests/VectorTests.cs
+++ SpaceBattle.Lib.Tests/VectorTests.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using StarWars.Lib;
 using Xunit;
 
-namespace SpaceBattle.Test;
+namespace StarWars.Test;
 
 public class VectorTests
 {
@@ -77,5 +77,49 @@ public class VectorTests
         var v1 = new Vector([1, 2]);
         var v2 = new Vector([1, 2]);
         Assert.Equal(v1.GetHashCode(), v2.GetHashCode());
+    }
+
+    [Fact]
+    public void VectorEqualityWithNullRightTest()
+    {
+        var v1 = new Vector([1, 2]);
+        Assert.False(v1 == null);
+    }
+
+    [Fact]
+    public void VectorEqualityWithNullLeftTest()
+    {
+        var v1 = new Vector([1, 2]);
+        Assert.False(null == v1);
+    }
+
+    [Fact]
+    public void VectorEqualsNullTest()
+    {
+        var v1 = new Vector([1, 2]);
+        Assert.False(v1.Equals(null));
+    }
+
+    [Fact]
+    public void VectorEqualsNonVectorTest()
+    {
+        var v1 = new Vector([1, 2]);
+        Assert.False(v1.Equals("string"));
+    }
+
+    [Fact]
+    public void VectorEqualsDifferentLengthTest()
+    {
+        var v1 = new Vector([1, 2]);
+        var v2 = new Vector([1, 2, 3]);
+        Assert.False(v1.Equals(v2));
+    }
+    [Fact]
+    public void VectorEqualityReferenceTest()
+    {
+#pragma warning disable CS1718
+        var v1 = new Vector([1, 2]);
+        Assert.True(v1 == v1);
+#pragma warning restore CS1718
     }
 }
