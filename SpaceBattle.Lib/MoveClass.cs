e5b1050e13091d4443b1dbbbf20c53b5032c8bc6
terracodum <nikitakharlov007@gmail.com>
2026-04-23 16:27:15 +0600

    feat(move): add MoveCommand and IMovingObject with tests

---
+++ SpaceBattle.Lib/MoveClass.cs
@@ -0,0 +1,23 @@
+﻿namespace StarWars.Lib;
+
+public interface IMovingObject
+{
+    Vector GetPosition();
+    Vector GetVelocity();
+    void SetPosition(Vector position);
+}
+
+public class MoveCommand : ICommand
+{
+    private readonly IMovingObject obj;
+
+    public MoveCommand(IMovingObject obj)
+    {
+        this.obj = obj;
+    }
+
+    public void Execute()
+    {
+        obj.SetPosition(obj.GetPosition() + obj.GetVelocity());
+    }
+}
