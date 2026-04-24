4fc2723b3b2a7ba7bb5adfe73331b0dbf4cce00c
terracodum <nikitakharlov007@gmail.com>
2026-04-25 00:52:04 +0600

    feat(actions): add RegisterIoCDependencyActionsStop and StopCommand with tests

---
+++ SpaceBattle.Lib.Tests/ActionStopIoCTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+using StarWars.Lib;
+using Xunit;
+
+namespace StarWars.Test;
+
+public class ActionsStopIoCTests
+{
+    public ActionsStopIoCTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        var iocScope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", iocScope).Execute();
+    }
+
+    [Fact]
+    public void Execute_ShouldRegisterStopCommandDependency()
+    {
+        var mockInjectable = new Mock<ICommandInjectable>();
+
+        var gameObject = new Dictionary<string, object>
+        {
+            ["repeatableMove"] = mockInjectable.Object
+        };
+        Hwdtech.ICommand injected = null;
+        mockInjectable.Setup(x => x.Inject(It.IsAny<Hwdtech.ICommand>())).Callback<Hwdtech.ICommand>(c => injected = c);
+
+        new RegisterIoCDependencyActionsStop().Execute();
+
+        var order = new Dictionary<string, object>
+        {
+            ["gameObject"] = gameObject,
+            ["cmdType"] = "Move"
+        };
+
+        var cmd = IoC.Resolve<Hwdtech.ICommand>("Actions.Stop", order);
+
+        cmd.Execute();
+        Assert.NotNull(cmd);
+
+        Assert.NotNull(injected);
+        injected.Execute();
+
+        mockInjectable.Verify(x => x.Inject(It.IsAny<Hwdtech.ICommand>()), Times.Once);
+    }
+}
---
+++ SpaceBattle.Lib/RegisterIoCDependencyActionsStop.cs
@@ -0,0 +1,18 @@
+﻿using Hwdtech;
+
+namespace StarWars.Lib;
+
+public class RegisterIoCDependencyActionsStop : Hwdtech.ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Actions.Stop", (object[] args) =>
+        {
+            var order = (IDictionary<string, object>)args[0];
+
+            return new StopCommand(
+                (IDictionary<string, object>)order["gameObject"],
+                (string)order["cmdType"]);
+        }).Execute();
+    }
+}
