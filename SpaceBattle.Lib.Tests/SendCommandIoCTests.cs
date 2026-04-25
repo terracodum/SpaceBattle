7924bf56b7e73775192303372b9b9ab4cd138372
smlx114 <alx444kv@gmail.com>
2026-04-25 12:34:30 +0600

    test: add test SendCommandIoC

---
+++ SpaceBattle.Lib.Tests/SendCommandIoCTests.cs
@@ -0,0 +1,31 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+using Xunit;
+using StarWars.Lib;
+
+namespace StarWars.Test;
+public class RegisterIoCDependencySendCommandTests
+{
+    public RegisterIoCDependencySendCommandTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+    }
+
+    [Fact]
+    public void Execute_ShouldRegisterSendCommandDependency()
+    {
+        var iocScope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", iocScope).Execute();
+
+        var mockCommand = new Mock<Hwdtech.ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        new RegisterIoCDependencySendCommand().Execute();
+
+        var sendCommand = IoC.Resolve<Hwdtech.ICommand>("Commands.Send", new object[] { mockCommand.Object, mockReceiver.Object });
+
+        Assert.NotNull(sendCommand);
+        Assert.IsType<SendCommand>(sendCommand);
+    }
+}
