6440b5b186eae7eaf60f32aee6bcf8be054a2011
smlx114 <alx444kv@gmail.com>
2026-04-25 12:58:31 +0600

    feat: add CommandInjectableCommand

---
+++ SpaceBattle.Lib/InjectableCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace StarWars.Lib;
+
+public interface ICommandInjectable
+{
+    void Inject(ICommand command);
+}
+public class CommandInjectableCommand : ICommand, ICommandInjectable
+{
+    private ICommand? _injectedCommand;
+
+    public CommandInjectableCommand() { }
+
+    public void Inject(ICommand command)
+    {
+        _injectedCommand = command;
+    }
+
+    public void Execute()
+    {
+        if (_injectedCommand == null)
+        {
+            throw new InvalidOperationException("Command not injected.");
+        }
+
+        _injectedCommand.Execute();
+    }
+}
