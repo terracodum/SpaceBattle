c1cbc6b8a0c396dc54d5a93fc13750fdc48eed6a
smlx114 <alx444kv@gmail.com>
2026-04-25 11:51:25 +0600

    test: add test SendCommand

---
+++ SpaceBattle.Lib.Tests/SendCommandTests.cs
@@ -0,0 +1,32 @@
+using Moq;
+using Xunit;
+using StarWars.Lib;
+using System;
+
+namespace StarWars.Test;
+
+public class SendCommandTests
+{
+    [Fact]
+    public void SendCommand_Execute_Test()
+    {
+        var cmd = new Mock<Hwdtech.ICommand>().Object;
+        var mockReceiver = new Mock<ICommandReceiver>();
+        var sendCommand = new SendCommand(cmd, mockReceiver.Object);
+
+        sendCommand.Execute();
+        mockReceiver.Verify(receiver => receiver.Receive(cmd), Times.Once(), "The Receive method was not called correctly.");
+    }
+
+    [Fact]
+    public void SendCommand_Receiver_Fails_Test()
+    {
+        var cmd = new Mock<Hwdtech.ICommand>().Object;
+        var mockReceiver = new Mock<ICommandReceiver>();
+        var sendCommand = new SendCommand(cmd, mockReceiver.Object);
+
+        mockReceiver.Setup(receiver => receiver.Receive(cmd)).Throws(new Exception());
+        var exception = Assert.Throws<Exception>(() => sendCommand.Execute());
+        Assert.IsType<Exception>(exception);
+    }
+}
