78c1e5f3282dd13f6851002a327fe9a7835952ea
terracodum <nikitakharlov007@gmail.com>
2026-06-06 14:40:15 +0600

    refactor: DictionaryAdapter delegates to IMethodHandler (OCP/LSP)

--- SpaceBattle.Lib/DictionaryAdapter.cs
+++ SpaceBattle.Lib/DictionaryAdapter.cs
@@ -6,38 +6,29 @@ public class DictionaryAdapter<T> : DispatchProxy where T : class
 {
     private IDictionary<string, object> _dictionary = null!;
     private IReadOnlyDictionary<string, Func<IDictionary<string, object>, object>> _strategies = null!;
+    private IReadOnlyList<IMethodHandler> _handlers = null!;
 
     public static T Create(
         IDictionary<string, object> dictionary,
-        IReadOnlyDictionary<string, Func<IDictionary<string, object>, object>> strategies)
+        IReadOnlyDictionary<string, Func<IDictionary<string, object>, object>> strategies,
+        IReadOnlyList<IMethodHandler> handlers)
     {
         var proxy = Create<T, DictionaryAdapter<T>>() as DictionaryAdapter<T>;
         proxy!._dictionary = dictionary;
         proxy._strategies = strategies;
+        proxy._handlers = handlers;
         return (T)(object)proxy;
     }
 
     protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
     {
         var name = targetMethod!.Name;
-
-        if (name.StartsWith("get_"))
+        var handler = _handlers.FirstOrDefault(h => h.CanHandle(name));
+        if (handler is not null)
         {
-            var prop = name[4..];
-            if (_strategies.TryGetValue(prop, out var strategy))
-            {
-                return strategy(_dictionary);
-            }
-
-            return _dictionary[prop];
+            return handler.Handle(name, _dictionary, _strategies, args);
         }
 
-        if (name.StartsWith("set_"))
-        {
-            _dictionary[name[4..]] = args![0]!;
-            return null;
-        }
-
-        throw new NotSupportedException($"Method '{name}' is not supported by DictionaryAdapter.");
+        throw new NotSupportedException($"No handler registered for method '{name}'.");
     }
 }
