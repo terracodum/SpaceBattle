058592f97231e4df557ed8c5c9026a8c4c678b6a
terracodum <nikitakharlov007@gmail.com>
2026-06-06 14:29:40 +0600

    feat: add DictionaryAdapter

---
+++ SpaceBattle.Lib/DictionaryAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+
+namespace StarWars.Lib;
+
+public class DictionaryAdapter<T> : DispatchProxy where T : class
+{
+    private IDictionary<string, object> _dictionary = null!;
+    private IReadOnlyDictionary<string, Func<IDictionary<string, object>, object>> _strategies = null!;
+
+    public static T Create(
+        IDictionary<string, object> dictionary,
+        IReadOnlyDictionary<string, Func<IDictionary<string, object>, object>> strategies)
+    {
+        var proxy = Create<T, DictionaryAdapter<T>>() as DictionaryAdapter<T>;
+        proxy!._dictionary = dictionary;
+        proxy._strategies = strategies;
+        return (T)(object)proxy;
+    }
+
+    protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
+    {
+        var name = targetMethod!.Name;
+
+        if (name.StartsWith("get_"))
+        {
+            var prop = name[4..];
+            if (_strategies.TryGetValue(prop, out var strategy))
+            {
+                return strategy(_dictionary);
+            }
+
+            return _dictionary[prop];
+        }
+
+        if (name.StartsWith("set_"))
+        {
+            _dictionary[name[4..]] = args![0]!;
+            return null;
+        }
+
+        throw new NotSupportedException($"Method '{name}' is not supported by DictionaryAdapter.");
+    }
+}
