08d76d950bf143fb5a42a9ec04f7ca292c7729fe
terracodum <nikitakharlov007@gmail.com>
2026-06-06 14:30:49 +0600

    feat: add RegisterAdapterDependency

---
+++ SpaceBattle.Lib/RegisterAdapterDependency.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Hwdtech;
+
+namespace StarWars.Lib;
+
+public class RegisterAdapterDependency<T> : ICommand where T : class
+{
+    private readonly Assembly _assembly;
+
+    public RegisterAdapterDependency(Assembly assembly)
+    {
+        _assembly = assembly;
+    }
+
+    public void Execute()
+    {
+        var strategies = _assembly
+            .GetCustomAttributes<AdapterAttribute>()
+            .Where(a => a.InterfaceType == typeof(T))
+            .ToDictionary(
+                a => a.PropertyName,
+                a => (Func<IDictionary<string, object>, object>)
+                    a.ResolverType
+                        .GetMethod("Resolve", BindingFlags.Public | BindingFlags.Static)!
+                        .CreateDelegate(typeof(Func<IDictionary<string, object>, object>))
+            );
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            $"Adapters.{typeof(T).Name}",
+            (Func<object, object>)(obj =>
+                DictionaryAdapter<T>.Create(
+                    (IDictionary<string, object>)((object[])obj)[0],
+                    strategies
+                )
+            )
+        ).Execute();
+    }
+}
