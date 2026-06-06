0716324ee4425385f789ff7da5d40225eacb8575
terracodum <nikitakharlov007@gmail.com>
2026-06-06 14:17:01 +0600

    feat: add CommandInjectableCommand

---
+++ SpaceBattle.Lib/InjectableCommand.cs
@@ -0,0 +1,25 @@
+using Hwdtech;
+
+namespace StarWars.Lib;
+
+public class CommandInjectableCommand : ICommand, ICommandInjectable
+{
+    private Hwdtech.ICommand? _injectedCommand;
+
+    public CommandInjectableCommand() { }
+
+    public void Inject(Hwdtech.ICommand command)
+    {
+        _injectedCommand = command;
+    }
+
+    public void Execute()
+    {
+        if (_injectedCommand == null)
+        {
+            throw new InvalidOperationException("Command not injected.");
+        }
+
+        _injectedCommand.Execute();
+    }
+}
