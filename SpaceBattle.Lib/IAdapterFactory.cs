ef9b58682e5c8cfd462f825d37267985fef143dc
terracodum <nikitakharlov007@gmail.com>
2026-06-06 14:42:02 +0600

    feat: add IAdapterFactory, DictionaryAdapterFactory, AdapterStrategyReader

---
+++ SpaceBattle.Lib/IAdapterFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace StarWars.Lib;
+
+public interface IAdapterFactory<T> where T : class
+{
+    T Create(
+        IDictionary<string, object> dictionary,
+        IReadOnlyDictionary<string, Func<IDictionary<string, object>, object>> strategies);
+}
